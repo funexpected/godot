82a567b3ce578aa5029de45a8aa86d7fcac1347f
Yasha Borevich <johndoe@example.com>
2018-09-04 11:45:48 +0300

    Do not reset controls's scale inside containers

--- scene/gui/container.cpp
+++ scene/gui/container.cpp
@@ -129,7 +129,7 @@ void Container::fit_child_in_rect(Control *p_child, const Rect2 &p_rect) {
 	p_child->set_position(r.position);
 	p_child->set_size(r.size);
 	p_child->set_rotation(0);
-	p_child->set_scale(Vector2(1, 1));
+	//p_child->set_scale(Vector2(1, 1));
 }
 
 void Container::queue_sort() {
