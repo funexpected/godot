5ea699c80713bf25e93fa1e2b7b0c9e5a6c33b77
Yasha Borevich <johndoe@example.com>
2018-09-07 14:45:52 +0300

    Add ios template build/install script

---
+++ scripts/install_ios_templates.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+VERSION=`python -c 'import version; print "%s.%s.%s" % (version.major, version.minor, version.patch if "patch" in dir(version) else version.status)'`
+TEMPLATES="$HOME/Library/Application Support/Godot/templates/$VERSION"
+if [ ! -d "$TEMPLATE" ]; then
+    mkdir "$TEMPLATES"
+fi
+
+scons p=iphone tools=no bits=64 target=release arch=arm64 module_firebase_enabled=no
+scons p=iphone tools=no bits=32 target=release arch=armv7 module_firebase_enabled=no
+lipo -create bin/libgodot.iphone.opt.arm.a bin/libgodot.iphone.opt.arm64.a -output bin/libgodot.iphone.release.fat.a
+rm -rf iphone*
+cp -R misc/dist/ios_xcode/ iphone/
+cp bin/libgodot.iphone.release.fat.a iphone/libgodot.iphone.release.fat.a
+cp bin/libgodot.iphone.release.fat.a iphone/libgodot.iphone.debug.fat.a
+zip -X -r iphone.zip iphone
+cp iphone.zip "$TEMPLATES"
+rm -rf iphone*
+
+
