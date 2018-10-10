265c054a99477236411f7a95d416fc970989be88
Ilya Busalaev <busalaev.games@gmail.com>
2018-10-10 10:36:37 +0300

    detach method

--- scene/main/node.h
+++ scene/main/node.h
@@ -265,6 +265,7 @@ public:
 
 	void add_child(Node *p_child, bool p_legible_unique_name = false);
 	void add_child_below_node(Node *p_node, Node *p_child, bool p_legible_unique_name = false);
+	void detach();
 	void reattach(Node *p_parent);
 	void remove_child(Node *p_child);
 
