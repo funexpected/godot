04b852a8f1b0fe24a49a23c281dd54c4be8b52ce
Yakov Borevich <j.borevich@gmail.com>
2018-12-05 18:45:28 +0300

    Add ios receipt validation & attributes parsing

--- platform/iphone/in_app_store.mm
+++ platform/iphone/in_app_store.mm
@@ -31,6 +31,20 @@
 #ifdef STOREKIT_ENABLED
 
 #include "in_app_store.h"
+#include "project_settings.h"
+#include "os/file_access.h"
+
+#include "openssl/bio.h"
+#include "openssl/x509v3.h"
+
+#include "mbedtls/asn1.h"
+
+/*
+#include "receipt/Payload.h"
+#include "receipt/ReceiptAttribute.h"
+#include "receipt/InAppReceipt.h"
+#include "receipt/InAppAttribute.h"
+*/
 
 extern "C" {
 #import <Foundation/Foundation.h>
@@ -70,6 +84,7 @@ void InAppStore::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("pop_pending_event"), &InAppStore::pop_pending_event);
 	ClassDB::bind_method(D_METHOD("finish_transaction"), &InAppStore::finish_transaction);
 	ClassDB::bind_method(D_METHOD("set_auto_finish_transaction"), &InAppStore::set_auto_finish_transaction);
+	ClassDB::bind_method(D_METHOD("get_payload"), &InAppStore::get_payload);
 };
 
 @interface ProductsDelegate : NSObject <SKProductsRequestDelegate> {
@@ -162,6 +177,265 @@ Error InAppStore::restore_purchases() {
 	return OK;
 };
 
+Dictionary InAppStore::get_payload(){
+	NSData *receipt = nil;
+	NSURL *receiptFileURL = nil;
+	NSBundle *bundle = [NSBundle mainBundle];
+	if ([bundle respondsToSelector:@selector(appStoreReceiptURL)]) {
+
+		// Get the transaction receipt file path location in the app bundle.
+		receiptFileURL = [bundle appStoreReceiptURL];
+
+		// Read in the contents of the transaction file.
+		receipt = [NSData dataWithContentsOfURL:receiptFileURL];
+	} 
+	if (receipt != nil) {
+		const void *_Nullable rawData = [receipt bytes];
+		char *data = (char *)rawData;
+		return InAppStore::_validate_payload(data, [receipt length]);
+	} else {
+		Dictionary res;
+		res["error"] = "Payload not available";
+		return res;
+	}
+
+}
+
+Dictionary InAppStore::_validate_payload(char* buff, int buff_size){
+	print_line("parsing receipt");
+	Dictionary res;
+	res["error"] = "Invalid receipt";
+	BIO *receiptBIO = BIO_new(BIO_s_mem());
+	BIO_write(receiptBIO, buff, buff_size);
+	PKCS7 *receiptPKCS7 = d2i_PKCS7_bio(receiptBIO, NULL);
+
+	ERR_FAIL_COND_V(!receiptPKCS7, res);
+	// Check that the container has a signature
+	ERR_FAIL_COND_V(!PKCS7_type_is_signed(receiptPKCS7), res);
+	// Check that the signed container has actual data
+	ERR_FAIL_COND_V(!PKCS7_type_is_data(receiptPKCS7->d.sign->contents), res);
+	print_line("Container  extracted");
+
+
+	
+	// Load the Apple Root CA (downloaded from https://www.apple.com/certificateauthority/)
+	res["error"] = "Can't load Apple Root Certificate";
+	String cert_path = ProjectSettings::get_singleton()->get("ios/root_certificate");
+	FileAccess *f = FileAccess::open(cert_path, FileAccess::READ);
+	ERR_FAIL_COND_V(!f, res);
+	PoolVector<uint8_t> cert_buff;
+	cert_buff.resize(f->get_len());
+	f->get_buffer(cert_buff.write().ptr(), f->get_len());
+
+
+	BIO *appleRootBIO = BIO_new(BIO_s_mem());
+	BIO_write(appleRootBIO, (const void *) cert_buff.read().ptr(), cert_buff.size());
+	X509 *appleRootX509 = d2i_X509_bio(appleRootBIO, NULL);
+
+	// Create a certificate store
+	X509_STORE *store = X509_STORE_new();
+	X509_STORE_add_cert(store, appleRootX509);
+	
+	// Be sure to load the digests before the verification
+	OpenSSL_add_all_digests();
+
+	// Check the signature
+	res["error"] = "Receipt verification failed";
+	int receipt_valid = PKCS7_verify(receiptPKCS7, NULL, store, NULL, NULL, 0);
+	ERR_FAIL_COND_V(receipt_valid != 1, res);
+
+	res["error"] = false;
+
+	// Parsing payload
+
+	// Date formatter to handle RFC 3339 dates in GMT time zone
+	NSDate *date = nil;
+	NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+	[formatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"]];
+	[formatter setDateFormat:@"yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"];
+	[formatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+	ASN1_OCTET_STRING *payload_octets = receiptPKCS7->d.sign->contents->d.data;
+	unsigned char *ptr = payload_octets->data;
+	unsigned char *end = ptr + payload_octets->length;
+	size_t len;
+	Array iap_receipts;
+	res["receipts"] = iap_receipts;
+	//ERR_FAIL_COND_V(mbedtls_asn1_get_len(&ptr, &end, &len), res);
+	//print_line(String("First tag: ") + itos(*ptr) );
+	ERR_FAIL_COND_V(mbedtls_asn1_get_tag(&ptr, end, &len, MBEDTLS_ASN1_SET|MBEDTLS_ASN1_CONSTRUCTED), res);
+	while (ptr < end) {
+		//print_line(String("Set tag: ") + itos(*ptr) );
+		ERR_FAIL_COND_V(mbedtls_asn1_get_tag(&ptr, end, &len, MBEDTLS_ASN1_SEQUENCE|MBEDTLS_ASN1_CONSTRUCTED), res);
+		int attr_type;
+		int attr_version;
+		ERR_FAIL_COND_V(mbedtls_asn1_get_int(&ptr, end, &attr_type), res);
+		ERR_FAIL_COND_V(mbedtls_asn1_get_int(&ptr, end, &attr_version), res);
+		ERR_FAIL_COND_V(mbedtls_asn1_get_tag(&ptr, end, &len, MBEDTLS_ASN1_OCTET_STRING), res);
+		//print_line(String("Checking attr type ") + itos(attr_type) + ", version " + itos(attr_version) + ", value size: " + itos(len)); 
+		switch (attr_type) {
+			case 2:
+				ERR_FAIL_COND_V(mbedtls_asn1_get_tag(&ptr, end, &len, MBEDTLS_ASN1_UTF8_STRING), res);
+				res["bundle_id"] = String::utf8((const char*)ptr, len);
+				ptr += len;
+				break;
+			case 3:
+				ERR_FAIL_COND_V(mbedtls_asn1_get_tag(&ptr, end, &len, MBEDTLS_ASN1_UTF8_STRING), res);
+				res["bundle_version"] = String::utf8((const char*)ptr, len);
+				ptr += len;
+				break;
+			case 17: {
+				//print_line("iap set tag " + itos(*ptr));
+				ERR_FAIL_COND_V(mbedtls_asn1_get_tag(&ptr, end, &len, MBEDTLS_ASN1_SET|MBEDTLS_ASN1_CONSTRUCTED), res);
+				unsigned char *iend = ptr + len;
+				Dictionary iap;
+				while (ptr < iend) {
+					//print_line("iap seq tag " + itos(*ptr));
+					ERR_FAIL_COND_V(mbedtls_asn1_get_tag(&ptr, iend, &len, MBEDTLS_ASN1_SEQUENCE|MBEDTLS_ASN1_CONSTRUCTED), res);
+					int iap_type;
+					int iap_version;
+					ERR_FAIL_COND_V(mbedtls_asn1_get_int(&ptr, iend, &iap_type), res);
+					ERR_FAIL_COND_V(mbedtls_asn1_get_int(&ptr, iend, &iap_version), res);
+					ERR_FAIL_COND_V(mbedtls_asn1_get_tag(&ptr, iend, &len, MBEDTLS_ASN1_OCTET_STRING), res);
+					//print_line(String("Checking iap_attr type ") + itos(iap_type) + ", version " + itos(iap_version) + ", value size: " + itos(len)); 
+					switch (iap_type){
+						case 1701: // quantity
+							int quantity;
+							ERR_FAIL_COND_V(mbedtls_asn1_get_int(&ptr, iend, &quantity), res);
+							iap["quantity"] = quantity;
+							break;
+						case 1702: // product id
+							ERR_FAIL_COND_V(mbedtls_asn1_get_tag(&ptr, iend, &len, MBEDTLS_ASN1_UTF8_STRING), res);
+							iap["product_id"] = String::utf8((const char*)ptr, len);
+							ptr += len;
+							break;
+						case 1703: // transaction id
+							ERR_FAIL_COND_V(mbedtls_asn1_get_tag(&ptr, iend, &len, MBEDTLS_ASN1_UTF8_STRING), res);
+							iap["transaction_id"] = String::utf8((const char*)ptr, len);
+							ptr += len;
+							break;
+						case 1705: // original transaction id
+							ERR_FAIL_COND_V(mbedtls_asn1_get_tag(&ptr, iend, &len, MBEDTLS_ASN1_UTF8_STRING), res);
+							iap["original_transaction_id"] = String::utf8((const char*)ptr, len);
+							ptr += len;
+							break;
+						case 1704: { // purchase date
+							ERR_FAIL_COND_V(mbedtls_asn1_get_tag(&ptr, iend, &len, MBEDTLS_ASN1_IA5_STRING), res);
+							NSString *dateString = [[NSString alloc] initWithBytes:ptr length:len encoding:NSASCIIStringEncoding];
+							date = [formatter dateFromString:dateString];
+							iap["purchase_date"] = String::utf8((const char*)ptr, len);
+							iap["purchase_date_ts"] = (float)[date timeIntervalSince1970];
+							ptr += len;
+							} break;
+						case 1706: { // original purchase date
+							ERR_FAIL_COND_V(mbedtls_asn1_get_tag(&ptr, iend, &len, MBEDTLS_ASN1_IA5_STRING), res);
+							NSString *dateString = [[NSString alloc] initWithBytes:ptr length:len encoding:NSASCIIStringEncoding];
+							date = [formatter dateFromString:dateString];
+							iap["original_purchase_date"] = String::utf8((const char*)ptr, len);
+							iap["original_purchase_date_ts"] = (float)[date timeIntervalSince1970];
+							ptr += len;
+							} break;
+						case 1708: { // subscription expiration_date
+							ERR_FAIL_COND_V(mbedtls_asn1_get_tag(&ptr, iend, &len, MBEDTLS_ASN1_IA5_STRING), res);
+							NSString *dateString = [[NSString alloc] initWithBytes:ptr length:len encoding:NSASCIIStringEncoding];
+							date = [formatter dateFromString:dateString];
+							iap["subscription_expiration_date"] = String::utf8((const char*)ptr, len);
+							iap["subscription_expiration_date_ts"] = (float)[date timeIntervalSince1970];
+							ptr += len;
+							} break;
+						case 1712: { // cancellation date
+							ERR_FAIL_COND_V(mbedtls_asn1_get_tag(&ptr, iend, &len, MBEDTLS_ASN1_IA5_STRING), res);
+							NSString *dateString = [[NSString alloc] initWithBytes:ptr length:len encoding:NSASCIIStringEncoding];
+							date = [formatter dateFromString:dateString];
+							iap["subscription_cancellation_date"] = String::utf8((const char*)ptr, len);
+							iap["subscription_cancellation_date_ts"] = (float)[date timeIntervalSince1970];
+							ptr += len;
+							} break;
+						//case 1711: // web order id
+						//	int web_order_id;
+						//	ERR_FAIL_COND_V(mbedtls_asn1_get_int(&ptr, iend, &web_order_id), res);
+						//	iap["web_order_id"] = web_order_id;
+						//	break;
+						default:
+							print_line("skip iap attr (type " + itos(iap_type) + ", tag " + itos(*ptr) + ")");
+							ptr += len;
+							break;
+					}
+				}
+				iap_receipts.push_back(iap);
+				} break;
+			default:
+				print_line("skip receipt attr");
+				ptr += len;
+				break;
+		}
+	}
+	
+	
+
+	//res["error"] = "Invalid receipt";
+	//ERR_FAIL_COND_V(bundleIdString == nil || bundleVersionString == nil || opaqueData == nil || hashData == nil, res);
+	
+	res["error"] = false;
+	//res["bundle_id"] = String::utf8([bundleIdString UTF8String]);
+	//res["bundle_version_string"] = String::utf8([bundleVersionString UTF8String]);
+	print_line("Receipt verified");
+
+
+
+
+
+	
+
+	//NSURL *appleRootURL = [[NSBundle mainBundle] URLForResource:@"AppleIncRootCertificate" withExtension:@"cer"];
+	//NSData *appleRootData = [NSData dataWithContentsOfURL:appleRootURL];
+	// BIO *appleRootBIO = BIO_new(BIO_s_mem());
+	// BIO_write(appleRootBIO, cert_buff.read().ptr(), cert_buff.size());
+	// X509 *appleRootX509 = d2i_X509_bio(appleRootBIO, NULL);
+
+	// // Create a certificate store
+	// X509_STORE *store = X509_STORE_new();
+	// X509_STORE_add_cert(store, appleRootX509);
+
+	// // Be sure to load the digests before the verification
+	// OpenSSL_add_all_digests();
+
+	// // Check the signature
+	// int result = PKCS7_verify(receiptPKCS7, NULL, store, NULL, NULL, 0);
+	// if (result != 1) {
+	// 	// Validation fails
+	// }
+	/*
+	print_line(String("parsing receipt payload, len:") + Variant((int)length));
+	Payload_t *payload = NULL;
+	asn_dec_rval_t rval;
+	rval = asn_DEF_Payload.ber_decoder(NULL, &asn_DEF_Payload, (void **)&payload, buff, length, 0);
+	print_line("payload parced, try to do attributs");
+	for (int i=0; i<payload->list.count; i++){
+		ReceiptAttribute_t *entry = payload->list.array[i];
+		print_line(String("Check Attr type ") + Variant((int)entry->type));
+		if (entry->type == 17){
+			void *rcp;
+			size_t rcp_size;
+			InAppReceipt_t *receipt;
+			asn_DEF_InAppReceipt.ber_decoder(NULL, &asn_DEF_InAppReceipt, (void**)&receipt, entry->value.buf, entry->value.size, 0);
+			for (int j=0; j<receipt->list.count; j++){
+				InAppAttribute_t *attr = receipt->list.array[j];
+				String attr_value = String((const CharType *)attr->value.buf, attr->value.size);
+				print_line(String("atrr type ") + Variant((int)attr->type) + ", attr value: " + attr_value);
+				
+				switch (attr->type){
+					int attr_value;
+					asn_DEF_INTEGER.ber_decoder(NULL, &asn_DEF_INTEGER, (void**)&attr_value, attr->value.buf, attr->value.size, 0);
+					case 1701: res["quantity"] = attr_value;
+				}
+				
+			}
+		}
+	}
+	*/
+	return res;
+}
+
 @interface TransObserver : NSObject <SKPaymentTransactionObserver> {
 };
 @end
@@ -184,44 +458,6 @@ Error InAppStore::restore_purchases() {
 				ret["result"] = "ok";
 				ret["product_id"] = pid;
 				ret["transaction_id"] = transactionId;
-
-				NSData *receipt = nil;
-				int sdk_version = 6;
-
-				if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0) {
-
-					NSURL *receiptFileURL = nil;
-					NSBundle *bundle = [NSBundle mainBundle];
-					if ([bundle respondsToSelector:@selector(appStoreReceiptURL)]) {
-
-						// Get the transaction receipt file path location in the app bundle.
-						receiptFileURL = [bundle appStoreReceiptURL];
-
-						// Read in the contents of the transaction file.
-						receipt = [NSData dataWithContentsOfURL:receiptFileURL];
-						sdk_version = 7;
-
-					} else {
-						// Fall back to deprecated transaction receipt,
-						// which is still available in iOS 7.
-
-						// Use SKPaymentTransaction's transactionReceipt.
-						receipt = transaction.transactionReceipt;
-					}
-
-				} else {
-					receipt = transaction.transactionReceipt;
-				}
-
-				NSString *receipt_to_send = nil;
-				if (receipt != nil) {
-					receipt_to_send = [receipt description];
-				}
-				Dictionary receipt_ret;
-				receipt_ret["receipt"] = String::utf8(receipt_to_send != nil ? [receipt_to_send UTF8String] : "");
-				receipt_ret["sdk"] = sdk_version;
-				ret["receipt"] = receipt_ret;
-
 				InAppStore::get_singleton()->_post_event(ret);
 
 				if (auto_finish_transactions) {
