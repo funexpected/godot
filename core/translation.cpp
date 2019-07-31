f70ab4177938d468329a1afdf5fa1ee9b2fb156f
Yakov Borevich <j.borevich@gmail.com>
2019-07-31 19:06:31 +0300

    [funexpected] better translatoin selection

--- core/translation.cpp
+++ core/translation.cpp
@@ -1029,6 +1029,10 @@ StringName TranslationServer::translate(const StringName &p_message) const {
 
 		const Ref<Translation> &t = E->get();
 		String l = t->get_locale();
+		if (l == locale) {
+			res = t->get_message(p_message);
+		}
+		/*
 		if (lptr[0] != l[0] || lptr[1] != l[1])
 			continue; // locale not match
 
@@ -1049,6 +1053,7 @@ StringName TranslationServer::translate(const StringName &p_message) const {
 			break;
 		else
 			near_match = true;
+		*/
 	}
 
 	if (!res) {
