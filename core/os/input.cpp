e03b05b5ffaf545c4fc1b7a1424c36c60295a09b
Shorin Sergey <serres123@ya.ru>
2020-04-17 18:00:41 +0300

    [funexpected] add keyboard_hidden signal to InputDefault on iOS

--- core/os/input.cpp
+++ core/os/input.cpp
@@ -123,6 +123,7 @@ void Input::_bind_methods() {
 	BIND_ENUM_CONSTANT(CURSOR_HELP);
 
 	ADD_SIGNAL(MethodInfo("joy_connection_changed", PropertyInfo(Variant::INT, "device"), PropertyInfo(Variant::BOOL, "connected")));
+	ADD_SIGNAL(MethodInfo("keyboard_hidden"));
 }
 
 void Input::get_argument_options(const StringName &p_function, int p_idx, List<String> *r_options) const {
