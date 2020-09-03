2d389d06e83af46d4a84f06be74ef34115e7a1e7
sergey-khrykov <sergey@funexpected.org>
2020-09-04 01:04:44 +0300

    Compile release template

--- scripts/install_android_templates.sh
+++ scripts/install_android_templates.sh
@@ -18,8 +18,9 @@ cd ../../..
 #scons platform=android target=debug android_arch=armv7
 
 # ARM v8 (64 bit)
-#scons platform=android target=release android_arch=arm64v8
-scons platform=android -j 4 target=debug android_arch=arm64v8 tools=no module_firebase_enabled=no module_bullet_enabled=no module_websocket_enabled=no game_center=no disable_3d=yes debug_symbols=yes separate_debug_symbols=yes
+scons platform=android target=release android_arch=arm64v8 tools=no module_firebase_enabled=no module_bullet_enabled=no module_websocket_enabled=no game_center=no disable_3d=yes debug_symbols=no
+
+scons platform=android -j 4 target=debug android_arch=arm64v8 tools=no module_firebase_enabled=no module_bullet_enabled=no module_websocket_enabled=no game_center=no disable_3d=yes debug_symbols=yes
 
 # x86 (32 bit)
 #scons platform=android target=release android_arch=x86
@@ -38,6 +39,6 @@ cd ../../..
 
 # Copy APKs to a standard template location
 cp bin/android_debug.apk "$TEMPLATES"
-#cp bin/android_release.apk "$TEMPLATES"
+cp bin/android_release.apk "$TEMPLATES"
 
 echo "$VERSION" > "$TEMPLATES/version.txt"
