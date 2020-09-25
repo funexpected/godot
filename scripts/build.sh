d9bb4f53514e6c658a0405684f93270fa9ac2084
Yakov Borevich <j.borevich@gmail.com>
2020-09-25 23:49:02 +0300

    [funexpected] add godot.headless

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. >/dev/null 2>&1 && pwd )"
 cd $DIR
-./scripts/install_editor.sh
-./scripts/install_ios_templates.sh
+./scripts/build_tools.sh
+./scripts/build_ios_templates.sh
