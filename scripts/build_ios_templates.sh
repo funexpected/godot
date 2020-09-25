3d0d3d2f5c8199083cb6c817e38c2b06507101e6
Yakov Borevich <j.borevich@gmail.com>
2020-09-25 23:49:02 +0300

    [funexpected] add godot.headless

--- scripts/build_ios_templates.sh
+++ scripts/build_ios_templates.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. >/dev/null 2>&1 && pwd )"
+cd $DIR
 rm -rf iphone*
 cp -R misc/dist/ios_xcode/ iphone/
 scons p=iphone -j 4 tools=no bits=64 target=release_debug arch=arm64 module_firebase_enabled=no module_bullet_enabled=no module_websocket_enabled=no game_center=no disable_3d=yes debug_symbols=yes separate_debug_symbols=yes || exit 1
--- scripts/install_editor.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-scons -j8 platform=osx
-cp bin/godot.osx.tools.64 executables/godot
-
-
