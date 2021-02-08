a8fece7828d9b800ecbbebe2e469f46fcdf05f86
Yakov Borevich <j.borevich@gmail.com>
2021-02-08 16:30:12 +0300

    [funexpected.ext] use Control as base class for VisibilityController

--- modules/fnxext/visibility_controller_2d.cpp
+++ modules/fnxext/visibility_controller_2d.cpp
@@ -8,25 +8,6 @@
 
 Rect2 VisibilityController2D::visible_rect = Rect2(Vector2(-50, -50), Vector2(100, 100));
 
-// void VisibilityController2D::_edit_set_rect(const Rect2 &p_edit_rect) {
-//     return Node2D::_edit_set_rect(p_edit_rect);
-// 	// rect = p_edit_rect;
-//     // // print_line(String("edit_set_rect ") + p_edit_rect, + " " +get_global_mouse_position());
-//     // if (Engine::get_singleton()->is_editor_hint()) {
-//     //     update();
-//     //     item_rect_changed();
-//     // }
-//     // _change_notify("rect");
-// }
-
-Rect2 VisibilityController2D::_edit_get_rect() const {
-    return visible_rect;
-}
-
-bool VisibilityController2D::_edit_use_rect() const {
-	return true;
-}
-
 bool VisibilityController2D::_get(const String &p_name, Variant &r_ret) const {
     if (p_name == "editor/canvas_layer") {
         r_ret = "visibility_controller";
@@ -63,24 +44,14 @@ void VisibilityController2D::set_control_activity(bool p_value) {
     update_visibility(true);
 }
 
-Rect2 VisibilityController2D::get_global_rect(){
-    Rect2 rect = visible_rect;
+Rect2 VisibilityController2D::get_bounding_box(){
+    Rect2 rect = get_rect();
     Transform2D tr = get_global_transform();
     Rect2 gr = Rect2(tr.xform(rect.position), Vector2());
     gr = gr.expand(tr.xform(rect.position + rect.size * Vector2(1, 0)));
     gr = gr.expand(tr.xform(rect.position + rect.size * Vector2(0, 1)));
     gr = gr.expand(tr.xform(rect.position + rect.size));
     return gr;
-}
-
-void VisibilityController2D::set_rect(const Rect2 &p_rect) {
-    set_position(p_rect.position + p_rect.size*0.5);
-    set_scale(p_rect.size / visible_rect.size);
-}
-
-Rect2 VisibilityController2D::get_rect() const {
-    Vector2 size = visible_rect.size * get_scale();
-    return Rect2(-size*0.5, size);
 }
 
 void VisibilityController2D::setup_controlled_node() {
@@ -109,18 +80,19 @@ void VisibilityController2D::update_visibility(bool force) {
     CanvasItem* item = Object::cast_to<CanvasItem>(get_node(controlled_node));
     if (item == NULL) return;
 
-    if (control_visibility) item->set_visible(visible);
+    
+    if (control_visibility) VS::get_singleton()->canvas_item_set_visible(item->get_canvas_item(), visible);
     if (control_activity) item->set_branch_paused(!visible);
 }
 VisibilityController2D::VisibilityType VisibilityController2D::detect_visibility_outside_parallax() {
     Rect2 vp_rect = get_viewport_rect();
-    Rect2 global_rect = get_global_rect();
+    Rect2 global_rect = get_bounding_box();
     return global_rect.intersects(vp_rect) ?  VisibilityType::VISIBLE : VisibilityType::INVISIBLE;
 }
 
 VisibilityController2D::VisibilityType VisibilityController2D::detect_visibility_inside_parallax() {
     Rect2 vpr = get_viewport_rect();
-    Rect2 fr = get_global_rect();
+    Rect2 fr = get_bounding_box();
     Vector2 num_parts = fr.size / vpr.size;
     num_parts.x = ceil(num_parts.x);
     num_parts.y = ceil(num_parts.y);
@@ -162,8 +134,7 @@ void VisibilityController2D::_notification(int p_what){
     if (Engine::get_singleton()->is_editor_hint()) {
 #ifdef TOOLS_ENABLED
         if (p_what == NOTIFICATION_DRAW && CanvasLayersEditorPlugin::get_singleton()->is_layer_visible("visibility_controller")) {
-            Rect2 rect = visible_rect;
-            draw_rect(rect, Color::html("3219f064"), true);
+            draw_rect(get_rect(), Color::html("3219f064"), true);
         }
 #endif
         return;
@@ -174,6 +145,7 @@ void VisibilityController2D::_notification(int p_what){
             update_visibility();
             set_notify_transform(true);
         }break;
+        case NOTIFICATION_RESIZED:
         case NOTIFICATION_TRANSFORM_CHANGED: {
             update_visibility();
         } break;
@@ -184,15 +156,14 @@ bool VisibilityController2D::is_visible_on_screen(){ return visibility; }
 
 void VisibilityController2D::_bind_methods(){
     ClassDB::bind_method(D_METHOD("is_visible_on_screen"), &VisibilityController2D::is_visible_on_screen);
-    ClassDB::bind_method(D_METHOD("get_global_rect"), &VisibilityController2D::get_global_rect);
-    ClassDB::bind_method(D_METHOD("get_rect"), &VisibilityController2D::get_rect);
-    ClassDB::bind_method(D_METHOD("set_rect", "rect"), &VisibilityController2D::set_rect);
+    ClassDB::bind_method(D_METHOD("get_bounding_box"), &VisibilityController2D::get_bounding_box);
     ClassDB::bind_method(D_METHOD("get_controlled_node"), &VisibilityController2D::get_controlled_node);
     ClassDB::bind_method(D_METHOD("set_controlled_node", "path"), &VisibilityController2D::set_controlled_node);
     ClassDB::bind_method(D_METHOD("should_control_visibility"), &VisibilityController2D::should_control_visibility);
     ClassDB::bind_method(D_METHOD("set_control_visibility", "value"), &VisibilityController2D::set_control_visibility);
     ClassDB::bind_method(D_METHOD("should_control_activity"), &VisibilityController2D::should_control_activity);
     ClassDB::bind_method(D_METHOD("set_control_activity", "value"), &VisibilityController2D::set_control_activity);
+    
     ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "controlled_node"), "set_controlled_node", "get_controlled_node");
     ADD_PROPERTY(PropertyInfo(Variant::BOOL, "control_visibility"), "set_control_visibility", "should_control_visibility");
     ADD_PROPERTY(PropertyInfo(Variant::BOOL, "control_activity"), "set_control_activity", "should_control_activity");
@@ -205,6 +176,7 @@ VisibilityController2D::VisibilityController2D(){
     controlled_node = NodePath("..");
     control_visibility = true;
     control_activity = false;
+    set_mouse_filter(MOUSE_FILTER_IGNORE);
     layer = NULL;
 }
 
