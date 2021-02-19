37ca29974056960c4debe0aa820998c1305856db
sergey-khrykov <sergey@funexpected.org>
2021-02-19 16:24:00 +0300

    [funexpected] Build iOS plugins during template build

--- scripts/build_ios_templates.sh
+++ scripts/build_ios_templates.sh
@@ -5,8 +5,12 @@ rm -rf iphone*
 cp -R misc/dist/ios_xcode/ iphone/
 scons p=iphone -j 4 tools=no bits=64 target=release_debug arch=arm64 module_firebase_enabled=no module_websocket_enabled=no game_center=no debug_symbols=yes separate_debug_symbols=yes || exit 1
 cp bin/libgodot.iphone.opt.debug.arm64.a iphone/libgodot.iphone.release.fat.a
-cp bin/libgodot_arkit_module.iphone.debug.arm64.a iphone/libgodot_arkit_module.release.fat.a
-cp bin/libgodot_camera_module.iphone.debug.arm64.a iphone/libgodot_camera_module.release.fat.a
+
+# Build iOS plugins (both Debug and Release)
+cd ios_plugins
+./scripts/release_static_library.sh 3.2
+cd ..
+
 zip -X -r iphone.zip iphone
 cp iphone.zip executables/ios.release.zip
 
@@ -14,8 +18,7 @@ rm -rf iphone*
 cp -R misc/dist/ios_xcode/ iphone/
 scons p=iphone -j 4 tools=no bits=64 target=debug arch=arm64 module_firebase_enabled=no module_websocket_enabled=no game_center=no debug_symbols=yes separate_debug_symbols=yes || exit 1
 cp bin/libgodot.iphone.debug.arm64.a iphone/libgodot.iphone.debug.fat.a
-cp bin/libgodot_arkit_module.iphone.opt.debug.arm64.a iphone/libgodot_arkit_module.debug.fat.a
-cp bin/libgodot_camera_module.iphone.opt.debug.arm64.a iphone/libgodot_camera_module.debug.fat.a
+
 zip -X -r iphone.zip iphone
 cp iphone.zip executables/ios.debug.zip
 rm -rf iphone*
