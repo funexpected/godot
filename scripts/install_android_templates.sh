61e844ec5a158357b2f3e9b8f6ea595621d2f8a2
Yakov Borevich <j.borevich@gmail.com>
2021-03-13 15:00:31 +0300

    [funexpected.scripts] some improvements to adndroid build script

--- scripts/install_android_templates.sh
+++ scripts/install_android_templates.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
-VERSION=`python -c 'import version; print "%s.%s%s.%s" % (version.major, version.minor, "."+str(version.patch) if "patch" in dir(version) else "", version.status)'`
+VERSION=`python -c 'import version; print("%s.%s%s.%s" % (version.major, version.minor, "."+str(version.patch) if "patch" in dir(version) else "", version.status))'`
 TEMPLATES="$HOME/Library/Application Support/Godot/templates/$VERSION"
 if [ ! -d "$TEMPLATE" ]; then
     mkdir "$TEMPLATES"
 fi
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 
 # Clean existing templates
 cd platform/android/java/
@@ -18,9 +19,10 @@ cd ../../..
 #scons platform=android target=debug android_arch=armv7
 
 # ARM v8 (64 bit)
-scons platform=android -j 4 target=release android_arch=arm64v8 tools=no module_firebase_enabled=no module_bullet_enabled=no module_websocket_enabled=no game_center=no disable_3d=yes
-
-scons platform=android -j 4 target=debug android_arch=arm64v8 verbose=yes tools=no module_firebase_enabled=no module_bullet_enabled=no module_websocket_enabled=no game_center=no disable_3d=yes
+# scons platform=android -j8 target=release android_arch=arm64v8 tools=no module_firebase_enabled=no module_bullet_enabled=no module_websocket_enabled=no game_center=no disable_3d=yes
+# scons platform=android -j8 target=debug android_arch=arm64v8 verbose=yes tools=no module_firebase_enabled=no module_bullet_enabled=no module_websocket_enabled=no game_center=no disable_3d=yes
+scons platform=android -j8 target=release android_arch=armv7 tools=no module_firebase_enabled=no module_bullet_enabled=no module_websocket_enabled=no game_center=no disable_3d=yes
+scons platform=android -j8 target=debug android_arch=armv7 verbose=yes tools=no module_firebase_enabled=no module_bullet_enabled=no module_websocket_enabled=no game_center=no disable_3d=yes
 
 # x86 (32 bit)
 #scons platform=android target=release android_arch=x86
