8b5d50f35111a193685e9c02a164f15dc6490e69
Shorin Sergey <serres123@ya.ru>
2021-05-31 17:14:58 +0300

    [funexpected][push_notification] init push notification, #1781

--- platform/iphone/ios.mm
+++ platform/iphone/ios.mm
@@ -34,6 +34,8 @@
 #include "core/project_settings.h"
 
 #import <UIKit/UIKit.h>
+#import <UIKit/UIKit.h>
+#import <UserNotifications/UserNotifications.h>
 #import "app_delegate.h"
 
 void iOS::_bind_methods() {
@@ -42,7 +44,8 @@ void iOS::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_interface_orientation"), &iOS::get_interface_orientation);
 	ClassDB::bind_method(D_METHOD("share_data", "text", "image_path"), &iOS::share_data, DEFVAL(""));
 	ClassDB::bind_method(D_METHOD("get_app_version"), &iOS::get_app_version);
-	
+	ClassDB::bind_method(D_METHOD("send_notification", "indifiter", "title", "body", "time_offset"), &iOS::send_notification);
+	ClassDB::bind_method(D_METHOD("cancel_notifications", "identifier_arr"), &iOS::cancel_notifications);
 };
 
 void iOS::alert(const char *p_alert, const char *p_title) {
@@ -109,6 +112,40 @@ String iOS::get_app_version()
 	const char	*str = [appVersionString UTF8String];
 	return String(str != NULL ? str : "");
 }
+
+
+void iOS::send_notification(const String &identifier_s, const String &title_s, const String &body_s, int time_offset){
+	UNMutableNotificationContent* content = [[UNMutableNotificationContent alloc] init];
+	content.title = [NSString stringWithCString:title_s.utf8().get_data() encoding:NSUTF8StringEncoding];
+	content.body = [NSString stringWithCString:body_s.utf8().get_data() encoding:NSUTF8StringEncoding];
+	NSString *identifier = [NSString stringWithCString:identifier_s.utf8().get_data() encoding:NSUTF8StringEncoding];
+
+	UNTimeIntervalNotificationTrigger* trigger = [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:time_offset repeats:NO];
+	UNNotificationRequest* request = [UNNotificationRequest requestWithIdentifier:identifier content:content trigger:trigger];
+	UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+	// print_line("Add notification with title: " + title_s + " and body: " + body_s);
+	[center addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+		if (error) {
+			const char *error_str = [error.description UTF8String];
+			ERR_PRINTS(String::utf8(error_str != NULL ? error_str : ""));
+		}
+	}];
+}
+
+
+void iOS::cancel_notifications(const Array &identifier_arr)
+{
+	NSMutableArray *result = [[[NSMutableArray alloc] init] autorelease];
+	for (unsigned int i = 0; i < identifier_arr.size(); ++i) {
+		String indifiter = identifier_arr[i];
+		NSString *identifier = [NSString stringWithCString:indifiter.utf8().get_data() encoding:NSUTF8StringEncoding];
+		[result addObject:identifier];
+	}
+	UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+	[center removePendingNotificationRequestsWithIdentifiers:result];
+	// print_line(String("Cancel notifications ") + (Variant)identifier_arr);
+}
+
 
 
 void iOS::share_data(const String &text, const String &image_path)
