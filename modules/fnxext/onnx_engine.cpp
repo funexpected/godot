377227ea901727254349b68fa3ecf9606b03a2cf
Shorin Sergey <serres123@ya.ru>
2022-12-23 01:06:20 +0400

    [fnx] fix  OnnxEngine

--- modules/fnxext/onnx_engine.cpp
+++ modules/fnxext/onnx_engine.cpp
@@ -3,7 +3,7 @@
 #include "onnx_engine.h"
 #include "core/print_string.h"
 #include "core/project_settings.h"
-
+#include "core/os/file_access.h"
 
 // #	var a = OnnxEngine.new()
 // #	a.load_from_file("res://mnist-12.onnx")
@@ -16,14 +16,6 @@ void OnnxEngine::_bind_methods() {
     ClassDB::bind_method(D_METHOD("load_from_file", "p_path"), &OnnxEngine::load_from_file);
 }
 
-String fix_res_path(String r_path) {
-    String resource_path = ProjectSettings::get_singleton()->get_resource_path();
-    if (resource_path != "") {
-        return r_path.replace("res:/", resource_path);
-    }
-    return r_path.replace("res://", "");
-
-}
 
 Variant OnnxEngine::set_input_layer(const String &layer_name) {
    input = onnx_tensor_search(ctx, layer_name.utf8().get_data());
@@ -37,8 +29,8 @@ Variant OnnxEngine::set_output_layer(const String &layer_name) {
    output = onnx_tensor_search(ctx, layer_name.utf8().get_data());
    if (output) {
         return Variant(true);
-   }
-        return Variant(false);
+    }
+    return Variant(false);
 }
 // input = onnx_tensor_search(ctx, "Input3");
 	// output = onnx_tensor_search(ctx, "Plus214_Output_0");
@@ -72,7 +64,9 @@ Array OnnxEngine::run(const Array& data) {
 
 
 Variant OnnxEngine::load_from_file(const String &file_path) {
-    ctx = onnx_context_alloc_from_file(fix_res_path(file_path).utf8().get_data(), NULL, 0);
+    Vector<uint8_t> onnx_data = FileAccess::get_file_as_array(file_path);
+
+    ctx = onnx_context_alloc(onnx_data.ptr(), onnx_data.size(), NULL, 0);
     
     if (ctx) {
         return Variant(true);
