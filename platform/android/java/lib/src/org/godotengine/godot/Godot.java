7ea3c8496099d24529f169225c32b74b22146487
Yakov Borevich <jkbo@funexpected.org>
2026-01-19 11:33:14 +0100

    fix some java

--- platform/android/java/lib/src/org/godotengine/godot/Godot.java
+++ platform/android/java/lib/src/org/godotengine/godot/Godot.java
@@ -107,6 +107,7 @@ import com.google.android.vending.expansion.downloader.IStub;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.security.MessageDigest;
